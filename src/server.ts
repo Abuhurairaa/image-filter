e68a5ea17fbb5b64119fa1e7fb6962af6e1bdb7c
Abuhurairaa <abuhurairaabubakar22@gmail.com>
2022-09-03 09:24:59 +0400

    Added filter image functionality to server.ts file.

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
  
   app.get("/filteredimage", async(req, res)=>{
-    const {image_url} = req.query.image_url.toString;
+    const {image_url} = req.query;
     if(!image_url){
       res.status(400).send("image_url is required")
     }
